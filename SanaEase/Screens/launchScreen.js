26193e274b464f87b0213b543976fae0982414de
Reagan Sagolsem <112538388+ReaganS369@users.noreply.github.com>
2023-11-14 22:11:54 +0530

    Done!

--- SanaEase/Screens/launchScreen.js
+++
@@ -1,7 +0,0 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { Image } from 'react-native-web';
-
-export default function LaunchScreen() {
-  return <div>main</div>;
-}
